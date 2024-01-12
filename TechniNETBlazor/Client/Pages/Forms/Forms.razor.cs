2961ac3b311b813474f34c6e16ad20813ee85cbe
BS-GavinC <98208007+VZGLY@users.noreply.github.com>
2024-01-12 11:26:49 +0100

    Forms

---
+++ TechniNETBlazor/Client/Pages/Forms/Forms.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Options;
+using System.Threading.Channels;
+using shared = TechniNETBlazor.Shared;
+
+namespace TechniNETBlazor.Client.Pages.Forms
+{
+    public partial class Forms
+    {
+
+        public shared.User monUser { get; set; } = new shared.User();
+
+
+        public void Validate()
+        {
+            Console.WriteLine(monUser.Email);
+        }
+
+        public void Invalidate()
+        {
+            Console.WriteLine("Fail");
+        }
+
+
+    }
+}
---
+++ TechniNETBlazor/Shared/Validators/NotUpperAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechniNETBlazor.Shared.Validators
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class NotUpperAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            string Value = (string)value;
+
+            foreach (char elem in Value)
+            {
+                if (char.IsUpper(elem))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
