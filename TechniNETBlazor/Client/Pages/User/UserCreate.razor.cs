a4b5359131d90c2b48295ec1661badba01a84121
BS-GavinC <98208007+VZGLY@users.noreply.github.com>
2024-01-12 08:49:47 +0100

    Ajoutez des fichiers projet.

---
+++ TechniNETBlazor/Client/Pages/User/UserCreate.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace TechniNETBlazor.Client.Pages.User
+{
+    public partial class UserCreate
+    {
+        public int MaProp { get; set; } = 12;
+
+        public string Value { get; set; }
+
+        public void ReceiveHello(string value)
+        {
+            this.Value = value;
+        }
+    }
+}
